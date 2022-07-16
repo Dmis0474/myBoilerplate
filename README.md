a9ed00a8651e6bf27017c59d06ccd278433f9953
unknown <Dmis0474@gmail.com>
2022-07-16 12:56:44 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# gulp-scss-boilerplate
+
+## Quick start in website development with HTML5/CSS3 using Sass/Scss and Javascript.
+
+### Tasks
+
+- compiling pre-processors to CSS
+- automatic page reload (browser-sync)
+- CSS files compression
+- images compression
+- create production build (available later in dist folder)
+
+### Quick start with boilerplate:
+
+```ssh
+git clone https://github.com/vtereshyn/gulp-scss-boilerplate.git
+cd gulp-scss-boilerplate;
+npm i
+npm run watch
+```
+
+### To build project
+
+```ssh
+npm run build
+```
+
+
+### template
+https://www.figma.com/file/vsXIsYNDXu0jUl9osztq5d/PrivateJetBooking?node-id=0%3A3
---
+++ gulpfile.js
@@ -0,0 +1,152 @@
+const del = require("del");
+const gulp = require("gulp");
+const scss = require("gulp-sass");
+const minify = require("gulp-minify");
+const cache = require("gulp-cache");
+const imagemin = require("gulp-imagemin");
+const browserSync = require("browser-sync");
+const pngquant = require("imagemin-pngquant");
+const inject = require("gulp-inject");
+
+const concat = require("gulp-concat");
+const rename = require("gulp-rename");
+const cssnano = require("gulp-cssnano");
+const autoprefixer = require("gulp-autoprefixer");
+
+gulp.task("img", () => {
+  gulp
+    .src("source/img/*")
+    // .pipe(
+    //   imagemin([
+    //     imagemin.gifsicle({ interlaced: true }),
+    //     imagemin.mozjpeg({ quality: 75, progressive: true }),
+    //     imagemin.optipng({ optimizationLevel: 5 }),
+    //     imagemin.svgo({
+    //       plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+    //     }),
+    //   ])
+    // )
+    .pipe(gulp.dest("public/img"));
+});
+
+gulp.task("vendor-styles", () =>
+  gulp
+    .src("source/vendor-styles/**.css", { allowEmpty: true })
+    .pipe(gulp.dest("public/css"))
+);
+
+gulp.task("vendor-scripts", () =>
+  gulp
+    .src("source/libs/**/*.js", { allowEmpty: true })
+    .pipe(gulp.dest("public/jsmin"))
+);
+
+gulp.task("sass", () =>
+  gulp
+    .src("source/scss/**/*.scss")
+    .pipe(scss({ outputStyle: "compressed" }))
+    .pipe(
+      autoprefixer(["last 15 versions", "> 1%", "ie 8", "ie 7", "ie 6"], {
+        cascade: true,
+      })
+    )
+    .pipe(browserSync.reload({ stream: true }))
+    .pipe(gulp.dest("public/css"))
+);
+
+gulp.task("jsmin", () =>
+  gulp
+    .src(["source/js/main.js"])
+    .pipe(
+      minify({
+        noSource: true,
+      })
+    )
+    .pipe(gulp.dest("public/jsmin"))
+);
+
+gulp.task("inject", () => {
+  var target = gulp.src("source/**/*.html");
+  var source = gulp.src(
+    [
+      "./public/jsmin/jquery/**/*.js",
+      "./public/jsmin/slick/**/*.js",
+      "./public/jsmin/**/*.js",
+      "public/css/**/*.css",
+    ],
+    {
+      read: false,
+    }
+  );
+  target
+    .pipe(inject(source, { ignorePath: "public" }))
+    .pipe(gulp.dest("source"));
+  
+});
+
+gulp.task("vendor-html", () =>
+  gulp
+  .src(["source/index.html"])
+  .pipe(gulp.dest("public/"))
+);
+
+gulp.task("browser-sync", () => {
+  browserSync({
+    server: {
+      baseDir: "public",
+    },
+    notify: false,
+  });
+});
+
+gulp.task("clear", (callback) => cache.clearAll());
+
+gulp.task(
+  "watch",
+  gulp.parallel(
+    "img",
+    "vendor-styles",
+    "vendor-scripts",
+    "sass",
+    "jsmin",
+    "inject",
+    "vendor-html",
+    "browser-sync",
+
+    () => {
+      gulp.watch("source/img/*", gulp.parallel("img"));
+      gulp.watch("source/vendor-styles/**.css", gulp.parallel("vendor-styles"));
+      gulp.watch("source/libs/**/*.js", gulp.parallel("vendor-scripts"));
+      gulp.watch("source/scss/**/*.scss", gulp.parallel("sass"));
+      gulp.watch("source/js/main.js", gulp.parallel("jsmin"));
+      gulp.watch("source/*.html", gulp.parallel("inject"));
+      gulp.watch("source/*.html", gulp.parallel("vendor-html"));
+    }
+  )
+);
+
+gulp.task("clean", () => del.sync("public"));
+
+gulp.task(
+  "build",
+  gulp.parallel(
+    "clean",
+    "img",
+    "vendor-styles",
+    "vendor-scripts",
+    "sass",
+    "jsmin",
+    "inject",
+    "vendor-html",
+
+    () => {
+      gulp.watch("source/img/*", gulp.parallel("img"));
+      gulp.watch("source/vendor-styles/**.css", gulp.parallel("vendor-styles"));
+      gulp.watch("source/libs/**/*.js", gulp.parallel("vendor-scripts"));
+      gulp.watch("source/scss/**/*.scss", gulp.parallel("sass"));
+      gulp.watch("source/js/main.js", gulp.parallel("jsmin"));
+      gulp.watch("source/*.html", gulp.parallel("inject"));
+      gulp.watch("source/*.html", gulp.parallel("vendor-html"));
+    }
+  )
+);
